c08b97f32562be27c79b97fd3c3c59adc65fefb9
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-10 18:53:21 +0200

    Create a function to add new todo, write the tasks to do on the website

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import streamlit as st
 from functions import get_todos, write_todos
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'w') as file:
+        pass
+
+todos = get_todos()
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] +'\n'
+    todos.append(new_todo)
+    write_todos(todos)
 
 st.title("My To-Do App")
 st.subheader("This app is to increase your productivity")
 
-st.text_input(label="", placeholder="Add new todo...", key="new_todo")
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo,  key="new_todo")
