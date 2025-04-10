eb0012a1fb2a879d43e69f938021b50dd0839197
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-10 18:57:21 +0200

    Add option to checked the tasks and remove them from the list

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ st.subheader("This app is to increase your productivity")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo,  key="new_todo")
